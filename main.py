0e92ac32f4b39f55540dbe656eefab50da8e86e6
Jason <xjasonlyu@gmail.com>
2020-04-30 15:02:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def parse_arguments():
     parser.add_argument('--debug', action='store_true',
                         help='enable debug mode')
     parser.add_argument('--version', action='version',
-                        version='%(prog)s v'+subscribe.version)
+                        version='%(prog)s v' + subscribe.version)
 
     args = parser.parse_args()
 
@@ -38,10 +38,13 @@ def main():
     subscribe.init(args.config)
 
     if args.debug:
-        print('* Debug mode: on')
         subscribe.app.debug = True
 
-    run_simple(args.bind, args.port, subscribe.app)
+    run_simple(args.bind, args.port,
+               subscribe.app, threaded=True,
+               use_debugger=args.debug, use_reloader=args.debug,
+               )
+
 
 if __name__ == '__main__':
     main()
