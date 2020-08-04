6f6875b67937ff43425900c820e96b877c63ca46
Jason <xjasonlyu@gmail.com>
2020-08-04 20:22:14 +0800

    Update filters.py

--- subscribe/filters.py
+++ subscribe/filters.py
@@ -171,10 +171,10 @@ def surge2clash(policy, *urls):
 
         # concat
         if _type in t:
-            _data = f'{_type},{_value},{policy}'
+            _data = f'{_type},{_value},{policy}'.strip(',')
         # replace
         elif _type in m:
-            _data = f'{m[_type]},{_value},{policy}'
+            _data = f'{m[_type]},{_value},{policy}'.strip(',')
         else:
             continue
 
